770aa0e5ce006734e98e585e0685f88a584900e5
pari93 <parichhaparitosh@gmail.com>
2026-04-06 14:49:31 +0200

    commit

--- IoT_Closed_Loop_Streaming_Project/Device/simulated_sensor.py
+++ IoT_Closed_Loop_Streaming_Project/Device/simulated_sensor.py
@@ -10,7 +10,7 @@ load_dotenv()
 # Put your device connection string here (or use env var)
 DEVICE_CONNECTION_STRING = os.getenv("DEVICE_CONNECTION_STRING")
 
-TARGET_TEMPERATURE = 35.0  # Desired temperature
+TARGET_TEMPERATURE = 15.0  # Desired temperature
 CURRENT_TEMPERATURE = 35.0 # Current temperature of the device [Change to >28 to see cooling in action or <18 to see heating in action immediately in the terminal logs]
 
 # Create a client that can send telemetry and receive twin updates
